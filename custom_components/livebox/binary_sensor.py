4469e3896ed1ed0b60f748030f9567b0d81a1c1b
cyr-ius <cyr-ius@ipocus.net>
2021-05-29 10:33:09 +0200

    Add call missed binary sensor

--- custom_components/livebox/binary_sensor.py
+++ custom_components/livebox/binary_sensor.py
@@ -6,8 +6,9 @@ from homeassistant.components.binary_sensor import (
     DEVICE_CLASS_CONNECTIVITY,
     BinarySensorEntity,
 )
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import COORDINATOR, DOMAIN, LIVEBOX_ID, TEMPLATE_SENSOR
+from .const import COORDINATOR, DOMAIN, LIVEBOX_ID
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -17,11 +18,13 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
     datas = hass.data[DOMAIN][config_entry.entry_id]
     box_id = datas[LIVEBOX_ID]
     coordinator = datas[COORDINATOR]
-    async_add_entities([WanStatus(coordinator, box_id)], True)
+    async_add_entities(
+        [WanStatus(coordinator, box_id), CallMissed(coordinator, box_id)], True
+    )
 
 
-class WanStatus(BinarySensorEntity):
-    """Representation of a livebox sensor."""
+class WanStatus(CoordinatorEntity, BinarySensorEntity):
+    """Wan status sensor."""
 
     device_class = DEVICE_CLASS_CONNECTIVITY
 
@@ -55,14 +58,16 @@ class WanStatus(BinarySensorEntity):
     def device_state_attributes(self):
         """Return the device state attributes."""
         wstatus = self.coordinator.data.get("wan_status", {}).get("data", {})
-        uptime = self.coordinator.data["infos"].get("UpTime")
+        uptime = datetime.today() - timedelta(
+            seconds=self.coordinator.data["infos"].get("UpTime")
+        )
         _attributs = {
             "link_type": wstatus.get("LinkType"),
             "link_state": wstatus.get("LinkState"),
             "last_connection_error": wstatus.get("LastConnectionError"),
             "wan_ipaddress": wstatus.get("IPAddress"),
             "wan_ipv6address": wstatus.get("IPv6Address"),
-            "uptime": ConvertSectoDay(uptime),
+            "uptime": uptime,
         }
         cwired = self.coordinator.data.get("count_wired_devices")
         if cwired > 0:
@@ -73,30 +78,36 @@ class WanStatus(BinarySensorEntity):
 
         return _attributs
 
-    @property
-    def available(self) -> bool:
-        """Return True if entity is available."""
-        return self.coordinator.last_update_success
+
+class CallMissed(CoordinatorEntity, BinarySensorEntity):
+    """Call missed sensor."""
+
+    def __init__(self, coordinator, box_id):
+        """Initialize the sensor."""
+        self.box_id = box_id
+        self.coordinator = coordinator
 
     @property
-    def should_poll(self):
-        """No polling needed."""
-        return False
+    def unique_id(self):
+        """Return unique_id."""
+        return f"{self.box_id}_callmissed"
 
-    async def async_added_to_hass(self):
-        """When entity is added to hass."""
-        await super().async_added_to_hass()
-        self.coordinator.async_add_listener(self.async_write_ha_state)
+    @property
+    def name(self):
+        """Return name sensor."""
+        return "Call missed"
 
-    async def async_will_remove_from_hass(self):
-        """When entity will be removed from hass."""
-        await super().async_will_remove_from_hass()
-        self.coordinator.async_remove_listener(self.async_write_ha_state)
+    @property
+    def is_on(self):
+        """Return true if the binary sensor is on."""
+        return len(self.coordinator.data.get("cmissed")) > 0
 
-    async def async_update(self) -> None:
-        """Update WLED entity."""
-        await self.coordinator.async_request_refresh()
+    @property
+    def device_info(self):
+        """Return the device info."""
+        return {"identifiers": {(DOMAIN, self.box_id)}}
 
-
-def ConvertSectoDay(n):
-    return datetime.today() - timedelta(seconds=n)
+    @property
+    def device_state_attributes(self):
+        """Return attributs."""
+        return self.coordinator.data.get("cmissed")
