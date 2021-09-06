3233f552021c05b6ea00065d0dfd0c1860163339
cyr-ius <cyr-ius@ipocus.net>
2021-09-06 09:04:22 +0200

    Move Tuple to const

--- custom_components/livebox/sensor.py
+++ custom_components/livebox/sensor.py
@@ -1,56 +1,12 @@
 """Sensor for Livebox router."""
 import logging
-from dataclasses import dataclass
-from typing import Final
 
-from homeassistant.components.sensor import (
-    STATE_CLASS_MEASUREMENT,
-    SensorEntity,
-    SensorEntityDescription,
-)
-from homeassistant.const import DATA_RATE_MEGABITS_PER_SECOND
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import COORDINATOR, DOMAIN, LIVEBOX_ID
+from .const import COORDINATOR, DOMAIN, LIVEBOX_ID, SENSOR_TYPES
 
 _LOGGER = logging.getLogger(__name__)
-
-
-@dataclass
-class FlowSensorEntityDescription(SensorEntityDescription):
-    """Represents an Flow Sensor."""
-
-    current_rate: str = None
-    attr: dict = None
-
-
-SENSOR_TYPES: Final[tuple[SensorEntityDescription, ...]] = (
-    FlowSensorEntityDescription(
-        key="down",
-        name="Orange Livebox Download speed",
-        current_rate="DownstreamCurrRate",
-        native_unit_of_measurement=DATA_RATE_MEGABITS_PER_SECOND,
-        state_class=STATE_CLASS_MEASUREMENT,
-        attr={
-            "downstream_maxrate": "DownstreamMaxRate",
-            "downstream_lineattenuation": "DownstreamLineAttenuation",
-            "downstream_noisemargin": "DownstreamNoiseMargin",
-            "downstream_power": "DownstreamPower",
-        },
-    ),
-    FlowSensorEntityDescription(
-        key="up",
-        name="Orange Livebox Upload speed",
-        current_rate="UpstreamCurrRate",
-        native_unit_of_measurement=DATA_RATE_MEGABITS_PER_SECOND,
-        state_class=STATE_CLASS_MEASUREMENT,
-        attr={
-            "upstream_maxrate": "UpstreamMaxRate",
-            "upstream_lineattenuation": "UpstreamLineAttenuation",
-            "upstream_noisemargin": "UpstreamNoiseMargin",
-            "upstream_power": "UpstreamPower",
-        },
-    ),
-)
 
 
 async def async_setup_entry(hass, config_entry, async_add_entities):
@@ -71,17 +27,10 @@ async def async_setup_entry(hass, config_entry, async_add_entities):
         async_add_entities(entities, True)
 
 
-class FlowSensor(SensorEntity):
+class FlowSensor(CoordinatorEntity, SensorEntity):
     """Representation of a livebox sensor."""
 
-    should_poll = False
-
-    def __init__(
-        self,
-        coordinator,
-        box_id,
-        description: SensorEntityDescription,
-    ):
+    def __init__(self, coordinator, box_id, description):
         """Initialize the sensor."""
         self.box_id = box_id
         self.coordinator = coordinator
@@ -117,17 +66,3 @@ class FlowSensor(SensorEntity):
         for key, value in self._attributs.items():
             _attributs[key] = self.coordinator.data["dsl_status"].get(value)
         return _attributs
-
-    async def async_added_to_hass(self):
-        """When entity is added to hass."""
-        await super().async_added_to_hass()
-        self.async_on_remove(
-            self.coordinator.async_add_listener(self.async_write_ha_state)
-        )
-
-    async def async_update(self):
-        """Update the entity.
-
-        Only used by the generic entity update service.
-        """
-        await self.coordinator.async_request_refresh()
