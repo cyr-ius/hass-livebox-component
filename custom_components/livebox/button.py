dd9e81111c48efbaa7c864b0ebea9804ca4719e7
chpego <38792705+chpego@users.noreply.github.com>
2021-12-30 08:03:14 +0100

    improve button code
    using ButtonDeviceClass class and EntityCategory

--- custom_components/livebox/button.py
+++ custom_components/livebox/button.py
@@ -1,9 +1,10 @@
 """Button for Livebox router."""
 import logging
 
-from homeassistant.components.button import ButtonEntity
+from homeassistant.components.button import ButtonDeviceClass, ButtonEntity
+from homeassistant.helpers.entity import EntityCategory
 
-from .const import DOMAIN, LIVEBOX_API, LIVEBOX_ID, RESTART_ICON
+from .const import DOMAIN, LIVEBOX_API, LIVEBOX_ID
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -19,13 +20,14 @@ class RestartButton(ButtonEntity):
     """Representation of a livebox sensor."""
 
     _attr_name = "Livebox restart"
-    _attr_icon = RESTART_ICON
 
     def __init__(self, box_id, api):
         """Initialize the sensor."""
         self.box_id = box_id
         self._api = api
         self._attr_unique_id = f"{self.box_id}_restart"
+        self._attr_device_class = ButtonDeviceClass.RESTART
+        self._attr_entity_category = EntityCategory.CONFIG
 
     @property
     def device_info(self):
