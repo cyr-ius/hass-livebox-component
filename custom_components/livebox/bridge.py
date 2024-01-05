112b0eed8fc9e5316d44fdd9d8df674dd91be304
cyr-ius <cyr-ius@ipocus.net>
2024-01-05 14:27:41 +0100

    Fix return is missing

--- custom_components/livebox/bridge.py
+++ custom_components/livebox/bridge.py
@@ -11,9 +11,10 @@ from aiosysbus.exceptions import (
     NotOpenError,
 )
 from homeassistant.util.dt import (
-    UTC,
     DEFAULT_TIME_ZONE,
+    UTC,
 )
+
 from .const import CALLID
 
 _LOGGER = logging.getLogger(__name__)
@@ -156,7 +157,9 @@ class BridgeData:
     async def async_get_device_schedule(self, device_key):
         """Get device schedule"""
         parameters = {"type": "ToD", "ID": device_key}
-        data = await self.async_make_request(self.api.schedule.get_schedule, **parameters)
+        data = await self.async_make_request(
+            self.api.schedule.get_schedule, **parameters
+        )
         if not isinstance(data, dict):
-            return False
+            return {}
         return data.get("data", {}).get("scheduleInfo", {})
