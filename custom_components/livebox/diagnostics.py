55397c29c1b849046627410c2cd38a170dde35d9
Benjamin Renard <brenard@zionetrix.net>
2024-01-05 18:26:20 +0100

    Fix pylint broad-exception-caught warning

--- custom_components/livebox/diagnostics.py
+++ custom_components/livebox/diagnostics.py
@@ -151,7 +151,7 @@ async def async_get_config_entry_diagnostics(
                     else f"Can't dump {str(type(result))} data"
                 )
             )
-        except Exception as err:
+        except Exception as err:  # pylint: disable=broad-exception-caught
             api_raw[api_method.__qualname__] = f"Exception: {err}"
     _LOGGER.debug("Diagnostics data builded in %0.1fs", time() - start_time)
 
