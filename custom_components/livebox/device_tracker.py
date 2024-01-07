d590af43ac62da6f94de6144c2e3b29a1ceaa341
LGO44 <95585425+LGO44@users.noreply.github.com>
2024-01-07 11:28:05 +0100

    Update device tracker.py
    get the files from the integration v1.9.0 and update the file device tracker from my previous version.
    
    add attrs["band"] = "Wired" if the connection is not wireless

--- custom_components/livebox/device_tracker.py
+++ custom_components/livebox/device_tracker.py
@@ -86,9 +86,109 @@ class LiveboxDeviceScannerEntity(
         """Return mac address."""
         return self.key
 
+# added LGO
+    @property
+    def link(self):
+        return (
+            self.coordinator.data.get("devices", {})
+            .get(self.unique_id, {})
+            .get("Interface")
+        )  
+
+    @property
+    def icon(self):
+        """Return icon."""    
+        
+        device = self.coordinator.data.get("devices", {}).get(self.unique_id, {}).get("DeviceType")   
+        if  device in ["Computer", "Desktop iOS","Desktop Windows","Desktop Linux" ]:
+            return "mdi:desktop-tower-monitor"    
+        elif device in ["Laptop", "Laptop iOS", "Laptop Windows","Laptop Linux"]:
+            return "mdi:laptop"
+        elif device in ["Switch4", "Switch8","Switch"  ]:
+            return "mdi:switch"
+        elif device in ["Acces Point",   ]:
+            return "mdi:access-point-network"
+        elif device in ["TV","TVKey","Apple TV" ]:
+            return "mdi:television"
+        elif device == "HomePlug":
+            return "mdi:network"    
+        elif device ==  "Printer":
+            return "mdi:printer"  
+        elif device in ["Set-top Box TV UHD", "Set-top Box"]:
+            return "mdi:dlna"          
+        elif device in ["Mobile iOS", "Mobile" ,"Mobile Android"]:
+            return "mdi:cellphone"    
+        elif device in ["Tablet iOS", "Tablet" ,"Tablet Android","Tablet Windows"]:
+            return "mdi:cellphone"   
+        elif device in ["Game Console", ]:
+            return "mdi:gamepad-square"   
+        elif device in  ["Homepoint"]:
+            return "mdi:home-automation"     
+        else:           
+            return "mdi:devices"    
+    
+                
+
+
+
+
+    @property
+    def device_info(self):
+        """Return the device info."""
+        return {
+            "name": self.name,
+            "identifiers": {(DOMAIN, self.unique_id)},
+            "via_device": (DOMAIN, self.box_id),
+        }
+
+            
     @property
     def extra_state_attributes(self):
         """Return the device state attributes."""
-        return {
-            "first_seen": self._device.get("FirstSeen"),
-        }
+        attrs = {}
+    #added by LGO    
+        attrs["scanner"] = "LiveboxDeviceScanner"
+        attrs["is_online"] = self._device.get("Active")
+        attrs["interface_name"] = self._device.get("InterfaceName")
+        attrs["ip_address"] = self._device.get("IPAddress")
+
+    #  connection ethernet wired or wifi      
+        if self._device.get("InterfaceName") in ["eth1" ,"eth2","eth3", "eth4", "eth5" ] :   
+            attrs["connection"] = "ethernet" 
+            attrs["is_wireless"] = False
+            attrs["band"] = "Wired"
+
+        else:    
+            if self._device.get("InterfaceName") in ["eth6","wlan0","wl0"]  :   
+                attrs["connection"] = "wifi" 
+                attrs["band"] = self._device.get("OperatingFrequencyBand")
+                attrs["signal_strength"] = self._device.get("SignalStrength")
+                attrs["is_wireless"] = True
+                # signal Quality
+                if self._device.get("SignalStrength") < -90 : 
+                    attrs["signal_quality"] ="very bad"
+                elif self._device.get("SignalStrength") <= -80 and self._device.get("SignalStrength") > -90 : 
+                    attrs["signal_quality"] ="bad"
+                elif self._device.get("SignalStrength") <= -70 and self._device.get("SignalStrength") > -80 : 
+                    attrs["signal_quality"] = "very low"
+                elif self._device.get("SignalStrength") <= -67 and self._device.get("SignalStrength") > -70 : 
+                    attrs["signal_quality"] = "low"
+                elif self._device.get("SignalStrength") <= -60 and self._device.get("SignalStrength") > -67 : 
+                    attrs["signal_quality"] = "good"
+                elif self._device.get("SignalStrength") <= -50 and self._device.get("SignalStrength") > -60 : 
+                    attrs["signal_quality"] = "very good"
+                elif self._device.get("SignalStrength") <= -30 and self._device.get("SignalStrength") > -50 : 
+                    attrs["signal_quality"] ="excellent"
+                elif self._device.get("SignalStrength") == 0 : 
+                    attrs["signal_quality"] ="unknown"    
+            else:
+                attrs["signal_quality"] ="unknown"  
+        
+        attrs["type"] = self._device.get("DeviceType")
+        attrs["vendor"] = self._device.get("VendorClassID")
+        attrs["manufacturer"] = self._device.get("Manufacturer")
+        attrs["first_seen"] = self._device.get("FirstSeen")
+        attrs["last_connection"] = self._device.get("LastConnection")
+        attrs["last_changed"] = self._device.get("LastChanged")
+
+        return attrs
