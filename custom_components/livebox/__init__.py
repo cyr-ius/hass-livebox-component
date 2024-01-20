ef3c4a379f351cd518feddec18411cd0611af092
cyr-ius <cyr-ius@ipocus.net>
2024-01-20 20:36:33 +0100

    Removing device creation

--- custom_components/livebox/__init__.py
+++ custom_components/livebox/__init__.py
@@ -2,13 +2,11 @@
 import logging
 
 import voluptuous as vol
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-from homeassistant.exceptions import PlatformNotReady
 from homeassistant.helpers import device_registry as dr
 
-from .const import CALLID, CONF_USE_TLS, DOMAIN, PLATFORMS
+from .const import CALLID, DOMAIN, PLATFORMS
 from .coordinator import LiveboxDataUpdateCoordinator
 
 CALLMISSED_SCHEMA = vol.Schema({vol.Optional(CALLID): str})
@@ -19,26 +17,11 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Livebox as config entry."""
     hass.data.setdefault(DOMAIN, {})
-    scheme = "https" if entry.data.get(CONF_USE_TLS) else "http"
 
     coordinator = LiveboxDataUpdateCoordinator(hass, entry)
     await coordinator.async_config_entry_first_refresh()
 
-    if (infos := coordinator.data.get("infos")) is None:
-        raise PlatformNotReady
-
     hass.data[DOMAIN][entry.entry_id] = coordinator
-
-    device_registry = dr.async_get(hass)
-    device_registry.async_get_or_create(
-        config_entry_id=entry.entry_id,
-        identifiers={(DOMAIN, coordinator.unique_id)},
-        manufacturer=infos.get("Manufacturer"),
-        name=infos.get("ProductClass", DOMAIN.capitalize()),
-        model=infos.get("ModelName"),
-        sw_version=infos.get("SoftwareVersion"),
-        configuration_url=f"{scheme}://{entry.data.get('host')}:{entry.data.get('port')}",
-    )
 
     entry.async_on_unload(entry.add_update_listener(_async_update_listener))
 
