78555891ffaa3aff0875c287cc071b738b64eb94
cyr-ius <cyr-ius@ipocus.net>
2024-01-21 20:03:07 +0100

    Rename entity_description

--- custom_components/livebox/entity.py
+++ custom_components/livebox/entity.py
@@ -19,24 +19,22 @@ class LiveboxEntity(CoordinatorEntity[LiveboxDataUpdateCoordinator], Entity):
     _attr_has_entity_name = True
 
     def __init__(
-        self,
-        coordinator: LiveboxDataUpdateCoordinator,
-        entity_description: EntityDescription,
+        self, coordinator: LiveboxDataUpdateCoordinator, description: EntityDescription
     ) -> None:
         """Initialize the entity."""
         super().__init__(coordinator)
-        self.entity_description = entity_description
+        self.entity_description = description
 
-        entry = coordinator.config_entry
+        config_entry = coordinator.config_entry
         infos = coordinator.data.get("infos", {})
-        scheme = "https" if entry.data.get(CONF_USE_TLS) else "http"
+        scheme = "https" if config_entry.data.get(CONF_USE_TLS) else "http"
 
-        self._attr_unique_id = f"{coordinator.unique_id}-{entity_description.key}"
+        self._attr_unique_id = f"{coordinator.unique_id}-{description.key}"
         self._attr_device_info = {
             "identifiers": {(DOMAIN, coordinator.unique_id)},
             "manufacturer": infos.get("Manufacturer"),
             "name": infos.get("ProductClass", DOMAIN.capitalize()),
             "model": infos.get("ModelName"),
             "sw_version": infos.get("SoftwareVersion"),
-            "configuration_url": f"{scheme}://{entry.data.get('host')}:{entry.data.get('port')}",
+            "configuration_url": f"{scheme}://{config_entry.data.get('host')}:{config_entry.data.get('port')}",
         }
