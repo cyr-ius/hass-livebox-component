ff526322fd21de9e1be2da73edc31dc149cbad08
cyr-ius <cyr-ius@ipocus.net>
2025-08-31 14:36:34 +0200

    Remove name device info , replace to integration's title

--- custom_components/livebox/entity.py
+++ custom_components/livebox/entity.py
@@ -1,15 +1,12 @@
 """Parent Entity."""
-from __future__ import annotations
 
-import logging
+from __future__ import annotations
 
 from homeassistant.helpers.entity import Entity, EntityDescription
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
 from .const import CONF_USE_TLS, DOMAIN
 from .coordinator import LiveboxDataUpdateCoordinator
-
-_LOGGER = logging.getLogger(__name__)
 
 
 class LiveboxEntity(CoordinatorEntity[LiveboxDataUpdateCoordinator], Entity):
@@ -33,7 +30,6 @@ class LiveboxEntity(CoordinatorEntity[LiveboxDataUpdateCoordinator], Entity):
         self._attr_device_info = {
             "identifiers": {(DOMAIN, coordinator.unique_id)},
             "manufacturer": infos.get("Manufacturer"),
-            "name": infos.get("ProductClass", DOMAIN.capitalize()),
             "model": infos.get("ModelName"),
             "sw_version": infos.get("SoftwareVersion"),
             "configuration_url": f"{scheme}://{config_entry.data.get('host')}:{config_entry.data.get('port')}",
